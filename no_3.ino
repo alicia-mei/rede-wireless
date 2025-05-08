53cd0db0166ee550eb62f28003e3bd8d649071ff
alicia-mei <117045780+alicia-mei@users.noreply.github.com>
2025-05-08 15:36:01 -0300

    Update no_3.ino
    added pwm

--- no_3.ino
+++ no_3.ino
@@ -1,13 +1,23 @@
 #include <RH_ASK.h>  // Inclui a biblioteca RadioHead para Amplitude Shift Keying
 #include <SPI.h>     // Inclui a biblioteca SPI necessária para a comunicação
 
-
+// Definir o pino do ESP32 para gerar PWM
+const int pwmPin = 17;  // É possível escolher outro pino, se necessário
 // Configuração do driver de RF para ESP32 com 433MHz (RX: GPIO4, TX: GPIO22)
 RH_ASK rf_driver(1000, 4, 22);  
 
 
 void setup() {
   Serial.begin(115200);  // Inicializa o monitor serial a 115200 baud rate
+  // Configurar o canal de PWM
+  // ledcSetup canal, frequência, resolução
+  ledcSetup(0, 125000, 8);  // Canal 0, frequência de 125 kHz, resolução de 8 bits (0-255)
+ 
+  // Atribuir o pino ao canal de PWM
+  ledcAttachPin(pwmPin, 0); // Atribui o pino ao canal 0
+
+  Serial.println("Gerando sinal PWM de 125 kHz...");
+  
   delay(4000);  // Atraso inicial para garantir a estabilização do sistema
   //Serial.println("ESP32 433MHz Transmitter and Receiver");
 
@@ -31,6 +41,9 @@ void setup() {
   rf_driver.setHeaderFrom(0x03);   // Remetente
   rf_driver.setHeaderId(0x03);     // ID da mensagem
   rf_driver.setHeaderFlags(0x00);  // Sem flags específicas
+
+  // Gerar um sinal PWM com ciclo de trabalho de 50% (valor 127 de 0 a 255)
+  ledcWrite(0, 127);  // Valor de 127 representa 50% de ciclo de trabalho (duty cycle)
 }
 
 
